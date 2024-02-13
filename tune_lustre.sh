30545674eeb2b01f8f9f13c5ffa1ce3456779703
Alexey Khodos <akhodos@tintri.com>
2024-02-13 12:40:53 -0800

    cleanup

--- tune_lustre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-lctl set_param osc.*.max_pages_per_rpc=16M
-lctl set_param osc.*.max_rpcs_in_flight=8
-lctl set_param osc.*.max_dirty_mb=512
-lctl set_param llite.*.max_read_ahead_mb=2048
